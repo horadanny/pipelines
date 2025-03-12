82df72041a7f7a27dbfa34fc46cdea0da16ef624
horadanny <danny.horanimana@datadrivin.com>
2025-03-13 00:15:42 +0100

    big change:D

--- ollama_pipeline.py
+++ ollama_pipeline.py
@@ -13,60 +13,50 @@ from llama_index.llms.ollama import Ollama
 from llama_index.llms.openai_like import OpenAILike
 from llama_index.core import Settings, VectorStoreIndex, PromptTemplate
 from llama_index.core.query_engine import CustomQueryEngine
+from llama_index.readers.json import JSONReader
 from llama_index.core.retrievers import BaseRetriever
 from schemas import OpenAIChatMessage
-from server.om_server import get_metadata_tables
+#from server.om_server import get_metadata_tables
 from typing import List, Union, Generator, Iterator
 import aiohttp
 import os
 
 from fastapi import FastAPI, HTTPException
-#from llama_index.core import Document
-from langchain.schema.document import Document
+from llama_index.core import Document
+#from langchain.schema.document import Document
 from pydantic import BaseModel
 import json
-import loggin
+#import loggin
 import requests
 import sys
 
 
 JWT_TOKEN = "eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvcGVuLW1ldGFkYXRhLm9yZyIsInN1YiI6ImluZ2VzdGlvbi1ib3QiLCJyb2xlcyI6WyJJbmdlc3Rpb25Cb3RSb2xlIl0sImVtYWlsIjoiaW5nZXN0aW9uLWJvdEBvcGVuLW1ldGFkYXRhLm9yZyIsImlzQm90Ijp0cnVlLCJ0b2tlblR5cGUiOiJCT1QiLCJpYXQiOjE3NDE0ODcwODUsImV4cCI6bnVsbH0.QeHxcKW7LQGaVVnGr9XyZJbvw3-k3ZF1_cHLqIWKyazaPNwsCs0feAphBSwDCs_MvwnWx_k0_hpOth8BBVPccvzv1ZMS-SxYoSF6TvONuchI0s9Z_8l7o4UJcJUQzi_AOjFssvxTwN1NDhZE7N6UX7yDM_9KcKZkS5O9MGNWOkfnqHtA7lM6L6-Yu5oVo9MGMU9PDycm8hQteIdStJXoPM7fXzK6IxghrFuqJ7upXLLqhuFWUyvm6y9H3mUX2Qhnjdbkj6bUx1hpugdWRXu_XWuJPS0WVehGBib2oiUgCFRet5GaLdzYhPke1U-TGBiDXnC5F7rUmplI-saR3hfoXA"
 
-
-def get_metadata_tables():
+async def get_metadata_tables():
     try:
-        # Ensure OpenMetadata server is initialized
-        #if not initialize_openmetadata_server():
-        #    raise HTTPException(status_code=500, detail="Failed to initialize OpenMetadata server")
-        # Ensure JWT_TOKEN is available
-        if not JWT_TOKEN:
-            raise HTTPException(status_code=500, detail="JWT_TOKEN is not set")
-
-        # Set up the headers with the JWT token
         headers = {
             "Authorization": f"Bearer {JWT_TOKEN}",
         }
-        # Make a GET request to the OpenMetadata API
-        response = requests.get(
-            "http://localhost:8585/api/v1/tables?limit=10",
-            headers=headers,
-            timeout=10  # 10 seconds timeout
-        )
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        metadata_json  = response.json()
-        #print(f"Metadata for tables json: {metadata_json}")
-        # Convert the JSON to a formatted string
-        metadata_str = json.dumps(metadata_json, indent=2)
-        #print(f"Metadata for tables json dumps: {metadata_str}")
-        table_document = [Document(page_content=metadata_str)]
-        #print(f"table document: {table_document}")
-        return table_document
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "http://localhost:8585/api/v1/tables?limit=10",
+                headers=headers,
+                timeout=10
+            ) as response:
+                response.raise_for_status()
+                return await response.json()
     
-    except requests.RequestException as e:
-        raise HTTPException(status_code=500, detail=f"Error fetching metadata: {str(e)}")
+    except aiohttp.ClientError as e:
+        return f"ClientConnectionError: {e}"
+    except aiohttp.ClientPayloadError as e:
+        return f"ClientPayloadError: {e}"
+    except aiohttp.ClientResponseError as e:
+        return f"ClientResponseError: {e}"
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
-    
+        return f"Unexpected error: {e}"
+
 
 class RAGStringQueryEngine(CustomQueryEngine):
     """RAG String Query Engine."""
@@ -101,7 +91,7 @@ class Pipeline:
         self.valves = self.Valves(
             **{
                 "LLAMAINDEX_OLLAMA_BASE_URL": os.getenv("LLAMAINDEX_OLLAMA_BASE_URL", "http://localhost:11434"),
-                "LLAMAINDEX_MODEL_NAME": os.getenv("LLAMAINDEX_MODEL_NAME", "llama3.2:1b"),
+                "LLAMAINDEX_MODEL_NAME": os.getenv("LLAMAINDEX_MODEL_NAME", "llama3.2:1"),
                 "LLAMAINDEX_EMBEDDING_MODEL_NAME": os.getenv("LLAMAINDEX_EMBEDDING_MODEL_NAME", "nomic-embed-text"),
             }
         )
@@ -116,12 +106,15 @@ class Pipeline:
             model=self.valves.LLAMAINDEX_MODEL_NAME,
             base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
         )
-
+        reader = JSONReader(levels_back=0, clean_json=True)
+        metadata_json = await get_metadata_tables()
         # This function is called when the server is started.
         global documents, index
 
-        self.documents = get_metadata_tables()
+        #self.documents = get_metadata_tables()
+        self.documents = reader.load_data(input_file=metadata_json, extra_info={})
         self.index = VectorStoreIndex.from_documents(self.documents)
+        self.retriever = self.index.as_retriever()
 
     async def on_shutdown(self):
         # This function is called when the server is stopped.
@@ -133,15 +126,15 @@ class Pipeline:
         # This is where you can add your custom RAG pipeline.
         # Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response.
 
-        qa_prompt = PromptTemplate(
-            "Context information is below.\n"
+        prompt_template = """"
+            Context information is below.\n"
             "---------------------\n"
             "{context_str}\n"
             "---------------------\n"
             "You are a helpful AI Assistant providing company specific knowledge to a new employee."
             "This knowledge is from metadata from the OpenMetadata server."
             Respond accurately, relying on the available metadata information.
-            "If the user asks about a specific table, database, or date, ensure you specify its name and reference relevant metadata when applicable"
+            "If the user asks about a specific table, database, or date, ensure you specify its name and reference relevant metadata when applicable."
             "Generate human readable output, avoid creating output with gibberish text."
             "Generate only the requested output, don't include any other language before or after the requested output."
             "Provide complete and detailed answers whenever possible"
@@ -152,12 +145,12 @@ class Pipeline:
             "answer the query.\n"
             "Query: {query_str}\n"
             "Answer: "
-        )
+        """
+        qa_prompt = PromptTemplate(template=prompt_template)
 
-        self.retriever = self.index.as_retriever()
         query_engine = RAGStringQueryEngine(
             retriever=self.retriever,
-            llm=Settings.llm,
+            llm=self.valves.LLAMAINDEX_MODEL_NAME,
             qa_prompt=qa_prompt,
         )
 
